0ad1ab0a736b44e0adc7eca09ce67e1b2f7aa578
johnjehiel <jnjehiel@gmail.com>
2025-05-09 15:39:21 +0530

    feat: Add ModelNewMenu for model import/link options
    This commit adds ModelNewMenu.tsx to the ModelManager header to allow users to import or link external GGUF models

---
+++ app/screens/ModelManager/ModelNewMenu.tsx
@@ -0,0 +1,52 @@
+import PopupMenu, { MenuRef } from '@components/views/PopupMenu'
+import { Model } from '@lib/engine/Local/Model'
+import { useState } from 'react'
+import { View } from 'react-native'
+
+type ModelNewMenuProps = {
+    modelImporting: boolean
+    setModelImporting: (b: boolean) => void
+}
+
+const ModelNewMenu: React.FC<ModelNewMenuProps> = ({ modelImporting, setModelImporting }) => {
+
+    const handleSetExternal = async (menuRef: MenuRef) => {
+        menuRef.current?.close()
+        if (modelImporting) return
+        setModelImporting(true)
+        await Model.linkModelExternal()
+        setModelImporting(false)
+    }
+
+    const handleImportModel = async (menuRef: MenuRef) => {
+        menuRef.current?.close()
+        if (modelImporting) return
+        setModelImporting(true)
+        await Model.importModel()
+        setModelImporting(false)
+    }
+
+    return (
+        <View>
+            <PopupMenu
+                placement="bottom"
+                icon="addfile"
+                disabled={modelImporting}
+                options={[
+                    {
+                        label: 'Copy Model Into KenkoAssist',
+                        icon: 'download',
+                        onPress: handleImportModel,
+                    },
+                    {
+                        label: 'Use External Model',
+                        icon: 'link',
+                        onPress: handleSetExternal,
+                    },
+                ]}
+            />
+        </View>
+    )
+}
+
+export default ModelNewMenu
