af73d3de69404f4a9b3e762c7aea8a005d8fb7aa
johnjehiel <jnjehiel@gmail.com>
2025-05-12 10:51:18 +0530

    feat: Add SamplerMenu screen
    Introduce SamplerMenu.tsx to allow users to manage sampler configurations

---
+++ app/SamplerMenu.tsx
@@ -0,0 +1,244 @@
+import DropdownSheet from '@components/input/DropdownSheet'
+import ThemedCheckbox from '@components/input/ThemedCheckbox'
+import ThemedSlider from '@components/input/ThemedSlider'
+import ThemedTextInput from '@components/input/ThemedTextInput'
+import Alert from '@components/views/Alert'
+import FadeDownView from '@components/views/FadeDownView'
+import HeaderButton from '@components/views/HeaderButton'
+import HeaderTitle from '@components/views/HeaderTitle'
+import PopupMenu from '@components/views/PopupMenu'
+import TextBoxModal from '@components/views/TextBoxModal'
+import { Samplers } from '@lib/constants/SamplerData'
+import { APISampler } from '@lib/engine/API/APIBuilder.types'
+import { localSamplerData } from '@lib/engine/LocalInference'
+import { useAppMode } from '@lib/state/AppMode'
+import { Logger } from '@lib/state/Logger'
+import { SamplersManager } from '@lib/state/SamplerState'
+import { Theme } from '@lib/theme/ThemeManager'
+import { useState } from 'react'
+import { ScrollView, StyleSheet, Text } from 'react-native'
+
+const SamplerMenu = () => {
+    const styles = useStyles()
+    const { spacing } = Theme.useTheme()
+    const { appMode } = useAppMode()
+    const [showNewSampler, setShowNewSampler] = useState<boolean>(false)
+
+    const {
+        addSamplerConfig,
+        deleteSamplerConfig,
+        changeConfig,
+        updateCurrentConfig,
+        currentConfigIndex,
+        currentConfig,
+        configList,
+    } = SamplersManager.useSamplers()
+
+    const getSamplerList = (): APISampler[] => {
+        if (appMode === 'local') return localSamplerData
+        return []
+    }
+
+    const handleDeleteSampler = () => {
+        if (configList.length === 1) {
+            Logger.errorToast(`Cannot Delete Last Configuration`)
+            return false
+        }
+
+        Alert.alert({
+            title: `Delete Sampler`,
+            description: `Are you sure you want to delete '${currentConfig.name}'?`,
+            buttons: [
+                { label: 'Cancel' },
+                {
+                    label: 'Delete Sampler',
+                    onPress: async () => {
+                        deleteSamplerConfig(currentConfigIndex)
+                    },
+                    type: 'warning',
+                },
+            ],
+        })
+        return true
+    }
+
+    const headerRight = () => (
+        <PopupMenu
+            icon="setting"
+            iconSize={24}
+            placement="bottom"
+            options={[
+                {
+                    label: 'Create Sampler',
+                    icon: 'addfile',
+                    onPress: (menu) => {
+                        setShowNewSampler(true)
+                        menu.current?.close()
+                    },
+                },
+                {
+                    label: 'Delete Sampler',
+                    icon: 'delete',
+                    onPress: (menu) => {
+                        if (handleDeleteSampler()) menu.current?.close()
+                    },
+                    warning: true,
+                },
+            ]}
+        />
+    )
+
+    return (
+        <FadeDownView style={{ flex: 1 }}>
+            <TextBoxModal
+                booleans={[showNewSampler, setShowNewSampler]}
+                onConfirm={(text: string) => {
+                    if (text === '') {
+                        Logger.errorToast(`Sampler name cannot be empty`)
+                        return
+                    }
+
+                    for (const item of configList)
+                        if (item.name === text) {
+                            Logger.errorToast(`Sampler name already exists.`)
+                            return
+                        }
+                    addSamplerConfig({ name: text, data: currentConfig.data })
+                }}
+            />
+
+            <HeaderTitle title="Samplers" />
+            <HeaderButton headerRight={headerRight} />
+
+            <DropdownSheet
+                containerStyle={{ marginHorizontal: spacing.xl, paddingVertical: spacing.m }}
+                selected={currentConfig}
+                data={configList}
+                onChangeValue={(item) => {
+                    if (item.name === currentConfig.name) return
+                    changeConfig(configList.indexOf(item))
+                }}
+                labelExtractor={(item) => item.name}
+            />
+
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                {currentConfig &&
+                    getSamplerList().map((item, index) => {
+                        const samplerItem = Samplers?.[item.samplerID]
+                        if (!samplerItem)
+                            return (
+                                <Text style={styles.unsupported}>
+                                    Sampler ID {`[${item.samplerID}]`} Not Supported
+                                </Text>
+                            )
+                        switch (samplerItem.inputType) {
+                            case 'slider':
+                                return (
+                                    (samplerItem.values.type === 'float' ||
+                                        samplerItem.values.type === 'integer') && (
+                                        <ThemedSlider
+                                            key={item.samplerID}
+                                            value={
+                                                currentConfig.data[samplerItem.internalID] as number
+                                            }
+                                            onValueChange={(value) => {
+                                                updateCurrentConfig({
+                                                    ...currentConfig,
+                                                    data: {
+                                                        ...currentConfig.data,
+                                                        [samplerItem.internalID]: value,
+                                                    },
+                                                })
+                                            }}
+                                            label={samplerItem.friendlyName}
+                                            min={samplerItem.values.min}
+                                            max={samplerItem.values.max}
+                                            step={samplerItem.values.step}
+                                            precision={samplerItem.values.precision ?? 2}
+                                        />
+                                    )
+                                )
+                            case 'checkbox':
+                                return (
+                                    <ThemedCheckbox
+                                        value={currentConfig.data[item.samplerID] as boolean}
+                                        key={item.samplerID}
+                                        onChangeValue={(b) => {
+                                            updateCurrentConfig({
+                                                ...currentConfig,
+                                                data: {
+                                                    ...currentConfig.data,
+                                                    [samplerItem.internalID]: b,
+                                                },
+                                            })
+                                        }}
+                                        label={samplerItem.friendlyName}
+                                    />
+                                )
+                            case 'textinput':
+                                return (
+                                    <ThemedTextInput
+                                        key={item.samplerID}
+                                        value={currentConfig.data[item.samplerID] as string}
+                                        onChangeText={(text) => {
+                                            updateCurrentConfig({
+                                                ...currentConfig,
+                                                data: {
+                                                    ...currentConfig.data,
+                                                    [item.samplerID]: text,
+                                                },
+                                            })
+                                        }}
+                                        label={samplerItem.friendlyName}
+                                    />
+                                )
+                            default:
+                                return (
+                                    <Text style={styles.warningText}>Invalid Sampler Field!</Text>
+                                )
+                        }
+                    })}
+            </ScrollView>
+        </FadeDownView>
+    )
+}
+
+export default SamplerMenu
+
+const useStyles = () => {
+    const { color, spacing } = Theme.useTheme()
+    return StyleSheet.create({
+        scrollContainer: {
+            paddingHorizontal: spacing.xl,
+            paddingVertical: spacing.xl2,
+            rowGap: spacing.xl,
+        },
+
+        dropdownContainer: {
+            marginHorizontal: spacing.xl,
+        },
+
+        button: {
+            padding: spacing.s,
+            borderRadius: spacing.s,
+            marginLeft: spacing.m,
+        },
+
+        warningText: {
+            color: color.text._100,
+            backgroundColor: color.error._500,
+            padding: spacing.m,
+            margin: spacing.xl,
+            borderRadius: spacing.m,
+        },
+
+        unsupported: {
+            color: color.text._400,
+            textAlign: 'center',
+            paddingVertical: spacing.m,
+            marginVertical: spacing.m,
+            borderRadius: spacing.m,
+            backgroundColor: color.neutral._300,
+        },
+    })
+}
